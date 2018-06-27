b18937e8a25bc60676b5bf5e7cd61877a90c6409
YashkinDmitry <yashkin23@gmail.com>
2018-06-27 15:37:07 +0300

    Add MainController + mapping on "/"

---
+++ src/main/java/ru/tsystems/mis/spring/MainController.java
@@ -0,0 +1,14 @@
+package ru.tsystems.mis.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/")
+    public String main(){
+        return "index";
+    }
+
+}
