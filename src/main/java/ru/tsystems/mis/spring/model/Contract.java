b6ea2cd0711e7abcb3ac8c689a7659fd93376ae4
YashkinDmitry <yashkin23@gmail.com>
2018-06-28 13:30:01 +0300

    Model of contract

--- src/main/java/ru/tsystems/mis/spring/model/Contract.java
+++ src/main/java/ru/tsystems/mis/spring/model/Contract.java
@@ -1,7 +1,8 @@
-package ru.tsystems.mis.spring.entity;
+package ru.tsystems.mis.spring.model;
 
 
 import javax.persistence.*;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -11,26 +12,65 @@ public class Contract {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
-    @Column(name = "phoneNamber", nullable = false, unique = true )
-    private int phoneNamber;
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
 
-    @Column(name = "tariff", nullable = false)
+    @Column(name = "phone_number", nullable = false, unique = true)
+    private String phoneNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "tariff_id")
     private Tariff tariff;
 
-    @Column(name = "optionList", nullable = false)
-    private Set<Option> optionList;
+    @ManyToMany
+    @JoinTable(name = "contract_connected_options",
+            joinColumns = @JoinColumn(name = "contract_id"),
+            inverseJoinColumns = @JoinColumn(name = "option_id"))
+    private List<Option> optionList;
+
+    @Column(name = "blocked_by_client", nullable = false)
+    private boolean blockedByClient;
+
+    @Column(name = "blocked_by_operator", nullable = false)
+    private boolean blockedByOperator;
 
     public Contract() {
     }
 
-    public int getPhoneNamber() {
-        return phoneNamber;
+    public Contract(Client client, String phoneNumber, Tariff tariff, List<Option> optionList, boolean blockedByClient, boolean blockedByOperator) {
+        this.client = client;
+        this.phoneNumber = phoneNumber;
+        this.tariff = tariff;
+        this.optionList = optionList;
+        this.blockedByClient = blockedByClient;
+        this.blockedByOperator = blockedByOperator;
     }
 
-    public void setPhoneNamber(int phoneNamber) {
-        this.phoneNamber = phoneNamber;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     public Tariff getTariff() {
@@ -41,12 +81,28 @@ public class Contract {
         this.tariff = tariff;
     }
 
-    public Set<Option> getOptionList() {
+    public List<Option> getOptionList() {
         return optionList;
     }
 
-    public void setOptionList(Set<Option> optionList) {
+    public void setOptionList(List<Option> optionList) {
         this.optionList = optionList;
+    }
+
+    public boolean isBlockedByClient() {
+        return blockedByClient;
+    }
+
+    public void setBlockedByClient(boolean blockedByClient) {
+        this.blockedByClient = blockedByClient;
+    }
+
+    public boolean isBlockedByOperator() {
+        return blockedByOperator;
+    }
+
+    public void setBlockedByOperator(boolean blockedByOperator) {
+        this.blockedByOperator = blockedByOperator;
     }
 
     @Override
@@ -54,22 +110,30 @@ public class Contract {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Contract contract = (Contract) o;
-        return phoneNamber == contract.phoneNamber &&
+        return blockedByClient == contract.blockedByClient &&
+                blockedByOperator == contract.blockedByOperator &&
+                Objects.equals(id, contract.id) &&
+                Objects.equals(client, contract.client) &&
+                Objects.equals(phoneNumber, contract.phoneNumber) &&
                 Objects.equals(tariff, contract.tariff) &&
                 Objects.equals(optionList, contract.optionList);
     }
 
     @Override
     public int hashCode() {
-
-        return Objects.hash(phoneNamber, tariff, optionList);
+        return Objects.hash(id, client, phoneNumber, tariff, optionList, blockedByClient, blockedByOperator);
     }
 
     @Override
     public String toString() {
         return "Contract{" +
-                "phoneNamber=" + phoneNamber +
-                ", tariff=" + tariff +
+                "id=" + id +
+                ", client=" + client.getName()+ " " + client.getSurename()+ " " +
+                ", phone number='" + phoneNumber + '\'' +
+                ", tariff=" + tariff.getTitle() +
+                ", connected options=" + optionList.size() +
+                ", blocked by client=" + blockedByClient +
+                ", blocked by operator=" + blockedByOperator +
                 '}';
     }
 }
